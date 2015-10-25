4d3ed6a35ab832fa93f955c75af78ccbccfa8e0d
Caleb Morris <caleb.morris.g@gmail.com>
2015-10-24 21:52:10 -0700

    Initial commit

---
+++ ResxEditor/Properties/AddinInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"ResxEditor", 
+	Namespace = "ResxEditor",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("ResxEditor")]
+[assembly:AddinCategory ("IDE extensions")]
+[assembly:AddinDescription ("ResxEditor")]
+[assembly:AddinAuthor ("calebmorris")]
+
---
+++ ResxEditor/src/DisplayBindings.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using MonoDevelop.Core;
+using MonoDevelop.Ide.Gui;
+using MonoDevelop.Projects;
+
+namespace ResxEditor
+{
+	public class DisplayBindings : IViewDisplayBinding
+	{
+
+		#region IViewDisplayBinding implementation
+
+		public IViewContent CreateContent (FilePath fileName, string mimeType, Project ownerProject)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public string Name {
+			get { return "Resx Editor"; }
+		}
+
+		#endregion
+
+		#region IDisplayBinding implementation
+
+		public bool CanHandle (FilePath fileName, string mimeType, Project ownerProject)
+		{
+			if (mimeType == "text/microsoft-resx" || fileName.Extension == ".resx") {
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		public bool CanUseAsDefault {
+//			get { return true; } // TODO
+			get { return false; }
+		}
+
+		#endregion
+		
+	}
+}
+
