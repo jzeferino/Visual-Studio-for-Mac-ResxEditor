b0b91c09156c6e9f378599c12a3f970c530dd2c4
Caleb Morris <caleb.morris.g@gmail.com>
2015-11-07 16:24:33 -0700

    Updated description

--- ResxEditor/Properties/AddinInfo.cs
+++ ResxEditor/Properties/AddinInfo.cs
@@ -9,6 +9,6 @@
 [assembly:AddinName ("ResxEditor")]
 [assembly:AddinCategory ("IDE extensions")]
 [assembly:AddinAuthor ("Caleb Morris")]
-[assembly:AddinDescription ("Resx editor for simplified resource handling")]
+[assembly:AddinDescription ("Resx editor for simplified resource handling.\n Currently only works with string resources.")]
 [assembly:AddinUrl("https://github.com/CalebMorris/Xamarin-ResxEditor")]
 [assembly:ImportAddinAssembly("ResxEditor.Core.dll")]
